311044164cbf776ae13eae2b76ec7db2472bdad5
Amit Singh Rana <ranaa7300@gmail.com>
2021-03-23 11:43:20 +0530

    Connect to MONGODB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// ENV Variables SETUP
 dotenv.config();
 
+///
+///
+/// MONGODB SETUP
+mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useUnifiedTopology:true})
+  .then(res => console.log("DB Connected"))
+  .catch(err => console.log(err));
+
+///
+///
+/// SERVER SETUP
 app.listen(process.env.PORT, () => {
   console.log(`Server running at PORT ${process.env.PORT}`);
 });
