4821f6b7320238185877502034d5c17ae97d964c
Amit Singh Rana <ranaa7300@gmail.com>
2021-03-23 14:09:28 +0530

    Generate User DB Model

---
+++ backend/db/UserModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  dob: Date,
+  profile: {
+    type: String,
+    default: ""
+  },
+  country: {
+    type: String,
+    default: "India"
+  },
+  dateOfJoin: {
+    type: Date,
+    default: Date.now
+  },
+  followers: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+  following: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+  posts: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Post"
+    }
+  ],
+  comments: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  likedPosts: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Post"
+    }
+  ],
+  likedComments: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  score: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
