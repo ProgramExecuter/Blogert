89f47f78aa4df704d1959cc3c64ce39465c8d5fc
Amit Singh Rana <ranaa7300@gmail.com>
2021-03-24 11:47:55 +0530

    Basic structure for Comment route

---
+++ backend/routes/CommentRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Create a new Comment
+router.get("/", (req, res) => {
+
+});
+
+// Edit a Comment
+router.post("/", (re, res) => {
+
+});
+
+module.exports = router;
