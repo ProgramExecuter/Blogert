7b07f3e179dacd729643f72beb2d2c956bf452f8
Amit Singh Rana <ranaa7300@gmail.com>
2021-03-24 11:45:23 +0530

    Basic structure for Post route

---
+++ backend/routes/PostRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Create a new Post
+router.get("/", (req, res) => {
+
+});
+
+// Edit a new Post
+router.post("/", (req, res) => {
+
+});
+
+module.exports = router;
