e86c146f60806d722120a2d6ae6494756a018b10
Amit Singh Rana <ranaa7300@gmail.com>
2021-03-24 11:42:25 +0530

    Basic structure for User route

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Create a new User
+router.get("/", (req, res) => {
+
+});
+
+// Edit the User Info
+router.post("/", (req, res) => {
+
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,11 @@ const app = express();
 // ENV Variables SETUP
 dotenv.config();
 
-///
-///
 /// MONGODB SETUP
 mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useUnifiedTopology:true})
   .then(res => console.log("DB Connected"))
   .catch(err => console.log(err));
 
-///
-///
 /// SERVER SETUP
 app.listen(process.env.PORT, () => {
   console.log(`Server running at PORT ${process.env.PORT}`);
