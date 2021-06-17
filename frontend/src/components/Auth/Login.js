fb43139186fa6937fa924e26681d0129c740eb37
Amit Singh Rana <ranaa73000@gmail.com>
2021-06-17 13:31:08 +0530

    Changed  class ->  className

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -4,17 +4,17 @@ import './Login.css';
 const Login = () => {
   return (
     <div className="container">
-      <div class="login">
+      <div className="login">
         <h1>Login</h1>
-        <div class="login__textbox">
-          <i class="fas fa-user"></i>
+        <div className="login__textbox">
+          <i className="fas fa-user"></i>
           <input type="text" placeholder="Username" name="input[username]"/>
         </div>
-        <div class="login__textbox">
-          <i class="fas fa-lock"></i>
+        <div className="login__textbox">
+          <i className="fas fa-lock"></i>
           <input type="password" placeholder="Password" name="input[password]"/>
         </div>
-        <input type="button" class="login__btn" value="Sign in" />
+        <input type="button" className="login__btn" value="Sign in" />
       </div>
     </div>
   );
