488f7e6f2e2669ba50ec455381da78767d4da4de
Amit Singh Rana <ranaa73000@gmail.com>
2021-06-25 12:32:48 +0530

    Major completion of user route

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,32 +1,110 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 import userDB from "../db/UserModel.js";
 
 const router = express.Router();
 
+const saltRounds = 10;
+
 
 // Create a new User
-router.post("/", (req, res) => {
-  res.send("New User Created");
+router.post("/", async (req, res) => {
+
+  await bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+    if(err) {
+      res.status(400).json(err);
+    }
+    else {
+      req.body.password = hash;
+      const user = new userDB(req.body);
+
+      await user.save()
+        .then(newUser => res.status(200).json(newUser))
+        .catch(err => res.status(400).json(err));
+    }
+  })
+  .catch(err => res.status(400).json(err));
 });
 
 
 // Get a particular User
-router.get("/:user_id", (req, res) => {
-  res.send("User Found with Id "+req.params.user_id);
+router.get("/:user_id", async (req, res) => {
+  
+  await userDB.findById(req.params.user_id, (err, foundUser) => {
+    if(err) {
+      res.status(404).json(err);
+    }
+    else {
+      res.status(200).json(foundUser);
+    }
+  });
+
 });
 
 
 // Edit a particular User Info
-router.put("/:user_id", (req, res) => {
-  res.send("User Info Edited");
+router.put("/:user_id", async (req, res) => {
+  let user = {};
+  if(req.body.email)
+    user.email = req.body.email;
+  if(req.body.name)
+    user.name = req.body.name;
+  if(req.body.status)
+    user.status = req.body.status;
+  if(req.body.dob)
+    user.dob = req.body.dob;
+  if(req.body.country)
+    user.country = req.body.country;
+  if(req.body.password)
+    user.password = req.body.password;
+
+  const updateDB = async () => {
+    await userDB.findByIdAndUpdate(req.params.user_id, {$set: user}, (err, updatedUser) => {
+      if(err) {
+        res.status(400).json(err);
+      }
+      else {
+        res.status(200).json(updatedUser);
+      }
+    })
+    .catch(err => (res.status.json(err)));
+  };
+
+  if(req.body.password)
+  {
+    await bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+      if(err) {
+        res.status(400).json(err);
+      }
+      else {
+        user.password = hash;
+        updateDB();
+      }
+    })
+    .catch(e => res.status(400).json(err));
+  }
+  else
+  {
+    updateDB();
+  }
+
 });
 
 
 // Delete a particular User
-router.delete("/:user_info", (req, res) => {
-  res.send("User Deleted");
+router.delete("/:user_id", async (req, res) => {
+  
+  await userDB.findByIdAndRemove(req.params.user_id, (err) => {
+    if(err) {
+      res.status(400).json(err);
+    }
+    else {
+      res.status(200).json({"message": "Deleted user"});
+    }
+
+  })
+
 });
 
 
